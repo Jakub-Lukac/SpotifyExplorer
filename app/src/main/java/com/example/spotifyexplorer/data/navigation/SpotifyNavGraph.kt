5e5bf707267e15cffb445db59c42c8ae48a46ebb
Jakub-Lukac <jakub.lukac81@gmail.com>
2025-11-13 00:15:18 +0000

    handling state for home screen added

--- app/src/main/java/com/example/spotifyexplorer/data/navigation/SpotifyNavGraph.kt
+++ app/src/main/java/com/example/spotifyexplorer/data/navigation/SpotifyNavGraph.kt
@@ -1,13 +1,16 @@
 package com.example.spotifyexplorer.data.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.spotifyexplorer.data.ui.about.AboutScreen
 import com.example.spotifyexplorer.data.ui.favoriteTracks.FavoriteTracksScreen
 import com.example.spotifyexplorer.data.ui.home.HomeScreen
+import com.example.spotifyexplorer.data.ui.home.HomeViewModel
 
 enum class SpotifyScreens() {
     Home,
@@ -24,7 +27,10 @@ fun SpotifyNavGraph(
         navController = navController,
         startDestination = SpotifyScreens.Home.name
     ) {
-        composable(SpotifyScreens.Home.name) { HomeScreen(modifier, navController) }
+        composable("home") { entry ->
+            val viewModel: HomeViewModel = viewModel(entry)
+            HomeScreen(modifier, navController, viewModel)
+        }
         composable(SpotifyScreens.FavoriteTracks.name) { FavoriteTracksScreen(navController) }
         composable(SpotifyScreens.About.name) { AboutScreen(navController) }
     }
