bbcba9a1f51b9e9f6f77a30bee01276d40da56f3
Jakub-Lukac <jakub.lukac81@gmail.com>
2025-11-27 20:18:06 +0000

    commnets added to data store

--- app/src/main/java/com/example/spotifyexplorer/data/datastore/TokenDataStore.kt
+++ app/src/main/java/com/example/spotifyexplorer/data/datastore/TokenDataStore.kt
@@ -9,15 +9,31 @@ import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
+
+// Creates DataStore instance using preferencesDataStore
+// As the documentation specifies it should be a top-level property
+// And give it a name of the file to be stored
+// Stores data in key-value format
+// Anywhere we have context we can access DataStore
 private val Context.dataStore by preferencesDataStore(name = "spotify_token_store")
 
 class TokenDataStore(private val context: Context) {
 
+    // Definition of key-value pairs to be stored
+    // companion object = static members of a class
+    // so there is no need to create an instance of the class
+    // it allows for direct access to the props of the class
     companion object {
+        // stringPreferencesKey = key for the string value
         val ACCESS_TOKEN = stringPreferencesKey("access_token")
+
+        // longPreferencesKey = key for the long value
         val EXPIRATION = longPreferencesKey("expiration_time")
     }
 
+    /**
+     * Saves the token and its expiration time in DataStore.
+     */
     suspend fun saveToken(token: String, expiresInMillis: Long) {
         context.dataStore.edit { prefs ->
             prefs[ACCESS_TOKEN] = token
@@ -25,6 +41,13 @@ class TokenDataStore(private val context: Context) {
         }
     }
 
+    /**
+     * Retrieves the access token from DataStore.
+     * Returns a Flow of String?
+     * Using Flow (state management) allows us to listen for changes in the data
+     * map { it (key) } to return the value of the key
+     * it - represents the preferences object
+     */
     val accessToken: Flow<String?>
         get() = context.dataStore.data.map { it[ACCESS_TOKEN] }
 
@@ -37,5 +60,4 @@ class TokenDataStore(private val context: Context) {
             prefs.remove(EXPIRATION)
         }
     }
-
 }
